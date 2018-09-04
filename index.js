5848585eb3a4b0ec71359b001d21fa8eab301ab7
Chris Hanson <blondyhanson@gmail.com>
2018-09-04 12:00:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string.toLowerCase)
-  return("I can't hear you!") } 
-  else { (string.toUpperCase)
-return("YES INDEED!") 
-}
+  return("I can't hear you!") }
+  else if(string.toUpperCase)
+  return("YES INDEED!")
+  else(string("I love you, Grandma."))
+  return("I love you,too.")
